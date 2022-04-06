6ffba3fc97ddd796bcd6999bbc22ead23b0f42b0
ASHRAY <jha.ashray12@gmail.com>
2022-04-06 19:42:46 +0530

    Added add to cart feature

--- src/components/collection-preview/collection-preview.jsx
+++ src/components/collection-preview/collection-preview.jsx
@@ -8,10 +8,11 @@ const CollectionPreview = ({title, items}) => (
     <div className='preview'>
     {items
     .filter((item, idx)=> idx < 4)
-    .map(({id,...otherItemProps}) => (
-    <CollectionItem key={id} {...otherItemProps}/>
-      ))
-    } </div>
+    .map(item => (
+      <CollectionItem key={item.id} item={item} />
+    ))}
+    
+    </div>
      </div>
 );
 
